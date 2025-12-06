396062e594563b295626e2e5282a4e9a03b86642
sandovalwilkerson705-dotcom <sandovalwilkerson705@gmail.com>
2025-12-06 10:44:36 -0400

    Actualizar prin-menu.js

--- plugins/prin-menu.js
+++ plugins/prin-menu.js
@@ -75,8 +75,8 @@ let handler = async (m, { conn, usedPrefix }) => {
 🎄 *${saludo}* 🎄
 ✨ Bienvenid@ al menú del bot ❄️
 ┗━━━━━━━━━━━━━━━━━━━┛\n`;
-> 👋 *Hola %name! %greeting…
 
+    let txt = intro +
       `🎅 Soy *Tech bot v1*, bot en desarrollo ${(conn.user.jid == global.conn.user.jid ? '(Principal 🅥)' : '(Sub-Bot 🅑)')}\n` +
       `🕒 *Hora:* ${timeStr}\n` +
       `📅 *Fecha:* ${dateStr}\n` +
