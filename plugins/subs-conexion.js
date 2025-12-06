d8d2e216e772e7406647ad3f4ddbf2d98fb47d9d
sandovalwilkerson705-dotcom <sandovalwilkerson705@gmail.com>
2025-12-06 18:00:45 -0400

    Actualizar subs-conexion.js

--- plugins/subs-conexion.js
+++ plugins/subs-conexion.js
@@ -1,4 +1,4 @@
-onst { useMultiFileAuthState, DisconnectReason, makeCacheableSignalKeyStore, fetchLatestBaileysVersion} = (await import("@whiskeysockets/baileys"));
+const { useMultiFileAuthState, DisconnectReason, makeCacheableSignalKeyStore, fetchLatestBaileysVersion} = (await import("@whiskeysockets/baileys"));
 import qrcode from "qrcode"
 import NodeCache from "node-cache"
 import fs from "fs"
