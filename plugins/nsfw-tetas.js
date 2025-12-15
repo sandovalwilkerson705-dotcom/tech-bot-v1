53211de8db0511db7664ab7c0e7118376260ca8f
sandovalwilkerson705-dotcom <sandovalwilkerson705@gmail.com>
2025-12-14 22:19:57 -0400

    Actualizar nsfw-tetas.js

--- plugins/nsfw-tetas.js
+++ plugins/nsfw-tetas.js
@@ -1,18 +1,1 @@
-let handler = async (m, { conn}) => {
-  let chat = global.db.data.chats[m.chat]; // ← accede a la config del grupo
-
-  if (!chat.nsfw) return m.reply('⚠️ La opción NSFW está desactivada en este grupo.');
-
-  let img = 'https://delirius-apiofc.vercel.app/nsfw/boobs';
-  let text = '*🫨 TETAS*';
-
-  await conn.sendMessage(m.chat, { image: { url: img}, caption: text}, { quoted: m});
-  m.react('✅');
-};
-
-handler.help = ['tetas'];
-handler.command = ['tetas'];
-handler.tags = ['nsfw'];
-handler.group = true; // ← asegúrate de que solo se use en grupos
-
-export default handler;
+[]
